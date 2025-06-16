02f2ffcb894daef3bee75f7bb5ed53ec80bd8c29
alfitrafadjri <alfitrafadjri00@gmail.com>
2025-06-16 14:51:54 +0700

    update configurasi google console

--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,12 +1,20 @@
-import { Helmet } from "react-helmet-async";
+import { Helmet } from 'react-helmet-async';
 
 export const Head = ({ title, description }) => {
-  const pageTitle = title ? `KMM-UNJ | ${title}` : "KMM-UNJ";
+  const pageTitle = title ? `KMM-UNJ | ${title}` : 'KMM-UNJ';
 
   return (
     <Helmet>
       <title>{pageTitle}</title>
+
       {description && <meta name="description" content={description} />}
+      <meta
+        property="og:description"
+        content="Bergabung bersama mahasiswa Minangkabau di UNJ untuk membangun dan melestarikan budaya."
+      />
+      <meta property="og:title" content="KMahasiswa Minangkabau UNJ" />
+      <meta property="og:url" content="https://kmm-unj.vercel.app/" />
+      <meta property="og:image" content="https://kmm-unj.vercel.app/preview.jpg" />
     </Helmet>
   );
 };
